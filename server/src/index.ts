d8a200805a50255bfe2cc473d6fd2b3e907a872c
Fayuzul <fayuzulhasnaat79@gmail.com>
2026-06-22 21:13:34 +0530

    shiting to typescript

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import type {Express} from 'express'
+
+
+
+const PORT = 8000
+const app:Express = express()
+
+app.listen(PORT, ():void =>{
+  console.log("Listening on port: ", PORT)
+})
--- server/src/lib/db.ts
+++ server/src/lib/db.ts
@@ -3,11 +3,11 @@ import mongoose from "mongoose"
 
 export const connectDb = async() => {
     try{
-        const db = await mongoose.connect(process.env.MONGO_URI)
+        const db = await mongoose.connect(process.env.MONGO_URI!)
         console.log(`MongoDb Connected : ${db.connection.host}`)
     } catch (err) {
-        console.log("Error connecting to MongoDb", error.message)
-        process.exist(1)
+        console.log("Error connecting to MongoDb", err)
+        process.exit(1)
 
     }
 }
